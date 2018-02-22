20d70dfde9157dd4dab25fbeb4baf685d8cf5f50
曹力升 <c_youxin@163.com>
2018-02-22 10:38:25 +0800

    build

--- src/src/components/profile/index.js
+++ src/src/components/profile/index.js
@@ -8,7 +8,8 @@ class InternalProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      frame1: false
+      frame1: false,
+      frame2: false
     };
   }
 
@@ -19,8 +20,15 @@ class InternalProfile extends Component {
           frame1: true
         });
         self.props.loaded();
+        setTimeout(this.frame2Start, 1000, self);
       }, 618, this);
     }
+  }
+
+  frame2Start(self) {
+    self.setState({
+      frame2: true
+    });
   }
 
   componentDidMount() {
@@ -36,12 +44,63 @@ class InternalProfile extends Component {
   }
 
   render() {
-    const { frame1 } = this.state;
+    const { frame1, frame2 } = this.state;
     return (
       <div className="container profile">
-        <div className="d-flex">
-          <h1 className={`first-title align-self-start ani ${frame1 ? '' : 'fade-in-right'}`}>曹力升</h1>
-          <h6 className={`first-title badge badge-warning ml-auto align-self-end ani ${frame1 ? '' : 'fade-in-left'}`}>中南大学2013年毕业生</h6>
+        <div className="row">
+          <div className="col-12 col-xl-4">
+            <div className="d-flex title">
+              <h1 className={`first-title align-self-start ani ${frame1 ? '' : 'fade-in-right'}`}>曹力升</h1>
+              <h6 className={`first-title badge badge-warning ml-auto align-self-end ani ${frame1 ? '' : 'fade-in-left'}`}>中南大学2013年毕业生</h6>
+            </div>
+          </div>
+          <div className={`d-none d-xl-block col-xl-8 ani ${frame2 ? '' : 'fade-in-up'}`}>
+            <h3 className="class-record-title">我的成绩单</h3>
+            <div class="table-responsive class-record-wrapper">
+              <table class="table table-bordered table-dark table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     );
