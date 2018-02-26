9a030e219d561d00f1a292381a19926f0535bf52
曹力升 <c_youxin@163.com>
2018-02-26 21:03:40 +0800

    点击查看详细内容初步

--- src/src/components/techtree/index.js
+++ src/src/components/techtree/index.js
@@ -7,6 +7,11 @@ import { nextAnimation, showIndicator } from '../../actions';
 import { wrapAnimation } from '../../utils';
 
 class InternalTechTree extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { focused: -1 };
+  }
+
   pleaseStartAnimation() {
     if (this.props.onload) {
       setTimeout((this.props.nextAnimation), 1000);
@@ -27,43 +32,40 @@ class InternalTechTree extends Component {
 
   render() {
     const { animations, showPager } = this.props;
+    const { focused } = this.state;
     return (
       <div className="container tech-tree-wrapper">
         <div className="row tech-tree-wrapper">
-          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'}`}
-            onClick={e => { }}
-            onTransitionEnd={e => wrapAnimation('techtree', 0, 4, 0, 1, showPager)} >
+          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'} ${focused === 1 ? 'focused' : ''}`}
+            onClick={e => { this.setState({ focused: focused === 0 ? 1 : 0 }); }}>
             <div className="horizontal bg line"></div>
             <div className="vertical bg line"></div>
-            <div className="horizontal ft ani line"></div>
-            <div className="vertical ft ani line"></div>
+            <div className="horizontal ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 0, 1, showPager) }}></div>
+            <div className="vertical ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 1, 1, showPager) }}></div>
             <span className="badge badge-warning">前端</span>
           </div>
-          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'}`}
-            onClick={e => { }}
-            onTransitionEnd={e => wrapAnimation('techtree', 0, 4, 1, 1, showPager)} >
+          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'} ${focused === 2 ? 'focused' : ''}`}
+            onClick={e => { this.setState({ focused: focused === 0 ? 2 : 0 }); }}>
             <div className="horizontal bg line"></div>
             <div className="vertical bg line"></div>
-            <div className="horizontal ft ani line"></div>
-            <div className="vertical ft ani line"></div>
+            <div className="horizontal ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 2, 1, showPager) }}></div>
+            <div className="vertical ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 3, 1, showPager) }}></div>
             <span className="badge badge-warning">后端</span>
           </div>
-          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'}`}
-            onClick={e => { }}
-            onTransitionEnd={e => wrapAnimation('techtree', 0, 4, 2, 1, showPager)} >
+          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'} ${focused === 3 ? 'focused' : ''}`}
+            onClick={e => { this.setState({ focused: focused === 0 ? 3 : 0 }); }}>
             <div className="horizontal bg line"></div>
             <div className="vertical bg line"></div>
-            <div className="horizontal ft ani line"></div>
-            <div className="vertical ft ani line"></div>
+            <div className="horizontal ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 4, 1, showPager) }}></div>
+            <div className="vertical ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 5, 1, showPager) }}></div>
             <span className="badge badge-warning">运维</span>
           </div>
-          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'}`}
-            onClick={e => { }}
-            onTransitionEnd={e => wrapAnimation('techtree', 0, 4, 3, 1, showPager)} >
+          <div className={`col-6 tech-tree mid box ${animations[0] ? '' : 'trans'} ${focused === 4 ? 'focused' : ''}`}
+            onClick={e => { this.setState({ focused: focused === 0 ? 4 : 0 }); }}>
             <div className="horizontal bg line"></div>
             <div className="vertical bg line"></div>
-            <div className="horizontal ft ani line"></div>
-            <div className="vertical ft ani line"></div>
+            <div className="horizontal ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 6, 1, showPager) }}></div>
+            <div className="vertical ft line" onTransitionEnd={e => { if (focused === -1) wrapAnimation('techtree', 0, 8, 7, 1, showPager) }} ></div>
             <span className="badge badge-warning">管理</span>
           </div>
         </div>
