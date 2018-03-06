2ccdfe0f479eaba3b4a8f71e350d638f196c7fd3
曹力升 <c_youxin@163.com>
2018-03-06 21:33:08 +0800

    update leetcode status

--- src/src/components/profile/index.js
+++ src/src/components/profile/index.js
@@ -46,9 +46,9 @@ class InternalProfile extends Component {
               <span className={`leetcode ani ${animations[2] ? '' : 'fade-in'}`}
                 onTransitionEnd={e => wrapAnimation('profile', 2, 1, 0, 1, nextAnimation)}><a href="https://leetcode.com/">LeetCode</a>进度</span>
               <span className={`badge easy ani ${animations[3] ? '' : 'fade-in'}`}
-                onTransitionEnd={e => wrapAnimation('profile', 3, 1, 0, 1, nextAnimation)}>Easy 29</span>
+                onTransitionEnd={e => wrapAnimation('profile', 3, 1, 0, 1, nextAnimation)}>Easy 34</span>
               <span className={`badge medium ani ${animations[4] ? '' : 'fade-in'}`}
-                onTransitionEnd={e => wrapAnimation('profile', 4, 1, 0, 1, nextAnimation)}>Medium 4</span>
+                onTransitionEnd={e => wrapAnimation('profile', 4, 1, 0, 1, nextAnimation)}>Medium 5</span>
               <span className={`badge hard ani ${animations[5] ? '' : 'fade-in'}`}
                 onTransitionEnd={this.props.showPager}>Hard 0</span>
             </div>
