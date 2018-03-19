94243cc7d295fe1d41de46ca78787d08fc31fbc4
曹力升 <c_youxin@163.com>
2018-03-19 12:59:15 +0800

    155 今が最高！！！！！

--- src/src/components/profile/index.js
+++ src/src/components/profile/index.js
@@ -46,7 +46,7 @@ class InternalProfile extends Component {
               <span className={`leetcode badge ani ${animations[2] ? '' : 'fade-in'}`}
                 onTransitionEnd={e => wrapAnimation('profile', 2, 1, 0, 1, nextAnimation)}><a href="https://leetcode.com/">LeetCode</a> Solved</span>
               <span className={`badge easy ani ${animations[3] ? '' : 'fade-in'}`}
-                onTransitionEnd={e => wrapAnimation('profile', 3, 1, 0, 1, nextAnimation)}>Easy 130</span>
+                onTransitionEnd={e => wrapAnimation('profile', 3, 1, 0, 1, nextAnimation)}>Easy 155</span>
               <span className={`badge medium ani ${animations[4] ? '' : 'fade-in'}`}
                 onTransitionEnd={e => wrapAnimation('profile', 4, 1, 0, 1, nextAnimation)}>Medium 8</span>
               <span className={`badge hard ani ${animations[5] ? '' : 'fade-in'}`}
